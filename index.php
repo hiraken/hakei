36410602f7df80bfd78049de9c98af5d60883bc6
hiraken <htb@mvf.biglobe.ne.jp>
2016-07-01 11:17:23 +0900

    Update index.php

--- index.php
+++ index.php
@@ -134,24 +134,8 @@ $err = 'Error Occurred';
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <LINK REL="SHORTCUT ICON" HREF="favicon.ico"> 
 <title>Hakei Player (MP3 Player with Sound Wave)</title>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-15214982-7']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script></title>
-
 <link href="css/style.css" type="text/css" rel="stylesheet">
 <link href="css/custom.css" type="text/css" rel="stylesheet">
-
-
 </head>
 <body>
 
@@ -223,16 +207,7 @@ ENCTYPE="MULTIPART/FORM-DATA">
                 <!-- end #mainContent -->
 
                 <hr>
-                                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Digital Dialogue -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-9003385815295706"
-     data-ad-slot="7792528073"
-     data-ad-format="auto"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+                               
 </div>            
 </td>
             </tr>       
